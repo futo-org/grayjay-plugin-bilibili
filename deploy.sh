08be50a726b3173e18a085264be56c77d914188c
Koen <koen@pop-os.localdomain>
2024-04-23 09:54:09 +0200

    Fixed casing.

--- deploy.sh
+++ deploy.sh
@@ -11,14 +11,14 @@ mkdir -p $DOCUMENT_ROOT/Bilibili
 cp build/BiliBiliIcon.png $DOCUMENT_ROOT/Bilibili
 cp build/BiliBiliConfig.json $DOCUMENT_ROOT/Bilibili
 cp build/BiliBiliScript.js $DOCUMENT_ROOT/Bilibili
-sh sign.sh $DOCUMENT_ROOT/Bilibili/BilibiliScript.js $DOCUMENT_ROOT/Bilibili/BilibiliConfig.json
+sh sign.sh $DOCUMENT_ROOT/Bilibili/BiliBiliScript.js $DOCUMENT_ROOT/Bilibili/BiliBiliConfig.json
 
 # Notify Cloudflare to wipe the CDN cache
 echo "Purging Cloudflare cache for zone $CLOUDFLARE_ZONE_ID..."
 curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
      -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
      -H "Content-Type: application/json" \
-     --data '{"files":["https://plugins.grayjay.app/Bilibili/BiliBiliIcon.png", "https://plugins.grayjay.app/Bilibili/BilibiliConfig.json", "https://plugins.grayjay.app/Bilibili/BilibiliScript.js"]}'
+     --data '{"files":["https://plugins.grayjay.app/Bilibili/BiliBiliIcon.png", "https://plugins.grayjay.app/Bilibili/BiliBiliConfig.json", "https://plugins.grayjay.app/Bilibili/BiliBiliScript.js"]}'
 
 # Take site back online
 echo "Bringing site back online..."
