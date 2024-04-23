aedea2359544cbc22f83e446eeb8c0c989454390
Koen <koen@pop-os.localdomain>
2024-04-23 09:51:11 +0200

    Added deploy scripts.

--- build/add-signature.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-public_key=$(head -1 "$1")
-sed -i -e 's|"scriptPublicKey": "",|'"$public_key"'|' "$2"
-
-signature=$(sed -n '2p' "$1")
-sed -i -e 's|"scriptSignature": "",|'"$signature"'|' "$2"
---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+DOCUMENT_ROOT=/var/www/sources
+
+# Take site offline
+echo "Taking site offline..."
+touch $DOCUMENT_ROOT/maintenance.file
+
+# Swap over the content
+echo "Deploying content..."
+mkdir -p $DOCUMENT_ROOT/Bilibili
+cp build/BiliBiliIcon.png $DOCUMENT_ROOT/Bilibili
+cp build/BiliBiliConfig.json $DOCUMENT_ROOT/Bilibili
+cp build/BiliBiliScript.js $DOCUMENT_ROOT/Bilibili
+sh sign.sh $DOCUMENT_ROOT/Bilibili/BilibiliScript.js $DOCUMENT_ROOT/Bilibili/BilibiliConfig.json
+
+# Notify Cloudflare to wipe the CDN cache
+echo "Purging Cloudflare cache for zone $CLOUDFLARE_ZONE_ID..."
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
+     -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+     -H "Content-Type: application/json" \
+     --data '{"files":["https://plugins.grayjay.app/Bilibili/BiliBiliIcon.png", "https://plugins.grayjay.app/Bilibili/BilibiliConfig.json", "https://plugins.grayjay.app/Bilibili/BilibiliScript.js"]}'
+
+# Take site back online
+echo "Bringing site back online..."
+rm $DOCUMENT_ROOT/maintenance.file
